9d14b0ca42f6fa0910adb0f1e7ea7bcac30c02fe
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-05-04 13:21:43 -0400

    Added messages for random welcome messages

--- messages.js
+++ messages.js
@@ -1,3 +1,6 @@
 [
-    
+    "welcome to the shit show! Hope you're ready!",
+    "you're now officially a cool cat!! But you've been chosen by idiots soooo",
+    "congrats on making it in with the funny squad!",
+    "hope you thought this through coming here! Just kidding we know you didn't and that's exactly why you're here🧡"
 ]
