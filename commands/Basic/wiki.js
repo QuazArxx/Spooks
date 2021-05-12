8fa56e0059c00adca175cb94b50873a739d73fa9
quazarxx <mysubs666@gmail.com>
2021-05-12 17:48:27 -0400

    Continuing the edits of the command

--- commands/Basic/wiki.js
+++ commands/Basic/wiki.js
@@ -1,27 +1,16 @@
 const Discord = require('discord.js');
 
 const { prefix } = require('../../config.json');
-const shortcuts = require('../../wikiShortcuts.json');
+const ghosts = require('../../ghosts.json');
+const equipment = require('../../equipment.json');
 
 module.exports = {
+    ghost: '',
+    tool: '',
     name: 'wiki',
     description: 'Posts the link to the Wiki Fandom',
     execute(message, args) {
         args = message.content.slice(prefix.length).split(' ');
-
-        let ghosts = [
-            'phantom', 'spirit', 'poltergeist', 'demon', 'wraith', 
-            'mare', 'revenant', 'banshee', 'oni', 'yurei', 'shade', 'jinn' 
-        ];
-
-        let equipment = [
-            'candle', 'crucifix', 'emf_reader', 'flashlight', 'ghost_writing_book',
-            'glow_stick', 'head_mounted_camera', 'infrared_light_sensor',
-            'lighter', 'motion_sensor', 'parabolic_microphone', 'photo_camera',
-            'salt_shaker', 'sanity_pills', 'smudge_sticks', 'sound_sensor', 'spirit_box',
-            'strong_flashlight', 'thermometer', 'tripod', 'uv_flashlight', 'video_camera'
-        ]
-
 
         // Posts the general link to the wiki if there's no argument
         if (!args[1]) {
@@ -35,22 +24,37 @@ module.exports = {
 
         // Sets argument to lowercase
         let choice = args[1].toLowerCase()
+    
+        // Empties the temporary variables
+        this.ghost = ''
+        this.tool = ''
 
-        // Changes shortcuts to one of the options
-        for (let x = 0; x < shortcuts.length; x++) {
-            if (shortcuts[x].shortcut == choice) {
-                choice = shortcuts[x].name
+        // Checks the ghost list to see if the choice is one of the options
+        for (let x = 0; x < ghosts.length; x++) {
+            if (ghosts[x].shortcut == choice || ghosts[x].lowercase == choice) {
+                this.ghost = ghosts[x].capitalize
                 break;
             }
         }
 
+
+        // Checks the equipment list to see if the choice is one of the options
+        if (this.ghost == '') {
+            for (let x = 0; x < equipment.length; x++) {
+                if (equipment[x].shortcut == choice || equipment[x].lowercase == choice) {
+                    this.tool = equipment[x].capitalize
+                    break;
+                }
+            }
+        }
+
         // Posts the link for the ghost or equipment the user chooses
-        if (ghosts.includes(choice) || equipment.includes(choice)) {
+        if (!(this.ghost == '') || !(this.tool == '')) {
             if (choice == 'wraith') {
                 const embed = new Discord.MessageEmbed()
                 .setColor('#000000')
                 .setTitle(`__Phasmophobia Wiki: ${choice.charAt(0).toUpperCase() + choice.slice(1)}__`)
-                .addField(`https://phasmophobia.fandom.com/wiki/${choice.charAt(0).toUpperCase() + choice.slice(1)}`, '*Also, according to Quaz, it\'s the only ghost that will kill you through a door.*')
+                .addField(`https://phasmophobia.fandom.com/wiki/${choice}`, '*Also, according to Quaz, it\'s the only ghost that will kill you through a door.*')
 
                 return message.channel.send(embed)
             }
