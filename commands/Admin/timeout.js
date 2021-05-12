a2524ccdbad7ef27b02287559cd5889494f43db2
quazarxx <mysubs666@gmail.com>
2021-05-12 17:48:12 -0400

    Started new timeout command

---
+++ commands/Admin/timeout.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'timeout',
+    description: 'Puts the person in timeout',
+    execute(message, args) {
+        // Push all information to members.json
+
+        // Remove all roles
+            // Check through every role the user has and remove it
+            // Add the timeout role
+    
+    }
+}
