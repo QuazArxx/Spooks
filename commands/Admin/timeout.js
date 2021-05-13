0fcfa4ce040fb71dd2d8651797100bd0eeda9db2
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-05-12 20:50:17 -0400

    Started new command

--- commands/Admin/timeout.js
+++ commands/Admin/timeout.js
@@ -1,8 +1,24 @@
+const fs = require('fs')
+
+const members = require('../../members.json')
+
 module.exports = {
     name: 'timeout',
     description: 'Puts the person in timeout',
     execute(message, args) {
+        // Pushes information of mentioned user if they're not already on the list
+        if (!members.some(user => user.id === message.mentions.users.first().id)) {
+            members.push({
+                name: message.mentions.users.first().username,
+                id: message.mentions.users.first().id,
+                roles: []
+            })
+        }
+        
         // Push all information to members.json
+        for (let x = 0; x < members.length; x++) {
+            if (!members) return
+        }
 
         // Remove all roles
             // Check through every role the user has and remove it
