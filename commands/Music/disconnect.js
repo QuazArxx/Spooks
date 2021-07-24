74a748596004b61e5b1da55a7bc2eeb138b6afd6
quazarxx <mysubs666@gmail.com>
2021-07-24 18:49:33 -0400

    Unneeded

--- commands/Music/disconnect.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'disconnect',
-    description: 'Allows Spooks to leave a voice channel',
-    aliases: 'dc',
-    async execute(message, args) {
-        const connection = await message.member.voice.channel.leave()
-    }
-}
