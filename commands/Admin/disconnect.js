e33d949b4783c34a35354762630b6808d4b8c5cc
quazarxx <mysubs666@gmail.com>
2021-07-26 10:50:49 -0400

    added ending dispatcher and empty queue

--- commands/Admin/disconnect.js
+++ commands/Admin/disconnect.js
@@ -1,14 +1,17 @@
 const Discord = require('discord.js')
 
 const colors = require('../../colors.json')
-
+const { queue } = require('../../functions')
 module.exports = {
     name: 'disconnect',
     description: 'forces the bot to leave the voice channel',
     permissions: 'ADMINISTRATOR',
     execute(message, args) {
         const connection = message.member.voice.channel
+        serverQueue = queue.get(message.guild.id)
 
+        serverQueue.songs = []
         connection.leave()
+        serverQueue.connection.dispatcher.end()
     }
 }
