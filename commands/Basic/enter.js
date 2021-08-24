af90a0241d8ecb6d9d8846c07c75076d52ba3e24
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-08-24 00:16:31 -0400

    Added a prevention if user is in competition

--- commands/Basic/enter.js
+++ commands/Basic/enter.js
@@ -3,14 +3,21 @@ const Discord = require('discord.js')
 
 const competition = require('../../competition.json')
 const functions = require('../../functions')
+const colors = require('../../colors.json')
 
 module.exports = {
     name: 'enter',
     execute(message, args) {
         if (functions.isThereCompetition == false || functions.areEntriesAllowed == false) {
             const embed = new Discord.MessageEmbed()
-            .setColor('#ff0000')
+            .setColor(colors.red)
             .setTitle('Either there is no competition or entries have been closed.')
+
+            return message.channel.send({ embeds: [embed] })
+        }else if (competition.some(user => user.id == message.author.id)) {
+            const embed = new Discord.MessageEmbed()
+            .setColor(colors.red)
+            .setTitle('You have already joined the competition. If you would like to leave, type "!leave"')
 
             return message.channel.send({ embeds: [embed] })
         }
@@ -18,13 +25,14 @@ module.exports = {
         competition.push({
             name: message.author.username,
             id: message.author.id,
+            isCaptain: false,
             object: message.member
         })
 
-        message.member.roles.add('853046476309528607')
+        message.member.roles.add('775547730901729330')
 
         const embed = new Discord.MessageEmbed()
-        .setColor('#00ff00')
+        .setColor(colors.green)
         .setTitle('You have entered the competition! Good luck!')
 
         fs.writeFile('./competition.json', JSON.stringify(competition), err => {
