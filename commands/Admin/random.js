ab109492b10ba77945cee9ac21df3e88dfe7fd51
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-08-28 00:36:41 -0400

    Made command to choose random teams

---
+++ commands/Admin/random.js
@@ -0,0 +1,15 @@
+// NEEDS TO BE FINISHED
+const fs = require('fs')
+const Discord = require('discord.js')
+
+const colors = require('../../colors.json')
+const teams = require('../../teams.json')
+const competition = require('../../competition.json')
+module.exports = {
+    name: 'random',
+    description: 'Picks random teams',
+    permissions: 'ADMINSTRATOR',
+    async execute(message, args) {
+
+    }
+}
