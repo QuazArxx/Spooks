b34f47521ee357b459619967e85c9a9a2a7e6530
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-09-01 18:48:50 -0400

    Finished fixes

--- commands/Admin/end.js
+++ commands/Admin/end.js
@@ -3,34 +3,31 @@ const fs = require('fs')
 
 const functions = require('../../functions')
 const competition = require('../../competition.json')
+const colors = require('../../colors.json')
 
 module.exports = {
     name: 'end',
     description: 'Ends the current competition and removes the competitor role from all participants.',
     permissions: 'ADMINISTRATOR',
     async execute(message, args) {
-        /*if (functions.isThereCompetition == false) {
+        if (functions.isThereCompetition == false) {
             const embed = new Discord.MessageEmbed()
-            .setColor('#ff0000')
+            .setColor(colors.red)
             .setTitle('There is no competition going on.')
             
             return message.channel.send({ embeds: [embed] })
-        }*/
+        }
 
         functions.isThereCompetition = false
 
         // Remove competitor role from participants
         for (let x = 0; x < competition.length; x++) {
-            try {
-                if (typeof message.guild.members.cache.get(competition[x].id) == 'undefined') {
-                    console.log(competition[x].object.displayName)
-                }
-            } catch (err) {
-                console.error(err)
-                return console.log(competition[x].object.displayName)
+            if (typeof message.guild.members.cache.get(competition[x].id) == 'undefined') {
+                continue
+            } else {
+                message.guild.members.cache.get(competition[x].id).roles.remove('853046476309528607')
             }
         }
-        return
 
         // Set the competition array to empty
         competition.length = 0
@@ -41,7 +38,7 @@ module.exports = {
 
         // Send a confirmation message
         const embed = new Discord.MessageEmbed()
-        .setColor('#00ff00')
+        .setColor(colors.green)
         .setTitle('The competition has officially ended.')
 
         await message.channel.send({ embeds: [embed] })
