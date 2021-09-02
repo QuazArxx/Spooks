d1bc6b01eec20fed7eb554fd2436d1a2d41e093f
quazarxx <mysubs666@gmail.com>
2021-09-02 14:32:15 -0400

    Finished for 1.0.0

--- commands/Competition/addcomp.js
+++ commands/Competition/addcomp.js
@@ -16,21 +16,47 @@ module.exports = {
             .setTitle('There is no current competition going on.')
 
             return message.channel.send({ embeds: [embed] })
+        } else if (!message.mentions.users.size) {
+            const embed = new Discord.MessageEmbed()
+            .setColor(colors.red)
+            .setTitle('You forgot to mention someone.')
+
+            return message.channel.send({ embeds: [embed] })
         }
-        // Add competitor role
+
         competition.push({
             name: message.mentions.users.first().username,
-            id: message.author.id,
+            id: message.mentions.users.first().id,
             isCaptain: false,
             object: message.member
         })
 
-        message.guild.members.cache.get(message.mentions.users.first().id).roles.add('877613773345681478')
-        // Add Team Role
-        // Add to playersPicked
+        playersPicked.push({
+            userId: message.mentions.users.first().id,
+            roleId: this.getRoleFromMention(args[1])
+        })
+
+        await message.guild.members.cache.get(message.mentions.users.first().id).roles.add('877613773345681478')
+        await message.guild.members.cache.get(message.mentions.users.first().id).roles.add(this.getRoleFromMention(args[1]))
 
         fs.writeFile('./PlayersPicked.json', JSON.stringify(playersPicked), err => {
             if (err) console.error(err);
         });
+
+        const embed = new Discord.MessageEmbed()
+        .setColor(colors.green)
+        .setTitle(`${message.mentions.users.first().username} was successfully added to the competition.`)
+
+        message.channel.send({ embeds: [embed] })
+    },
+    
+    getRoleFromMention: function (mention) {
+        if (!mention) return;
+    
+        if (mention.startsWith('<@&') && mention.endsWith('>')) {
+            mention = mention.slice(3, -1);
+    
+            return mention;
+        }
     }
 }
