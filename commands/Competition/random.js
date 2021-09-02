55793268f2257783be252a1aba61f5f45d1485a2
quazarxx <mysubs666@gmail.com>
2021-09-02 14:32:52 -0400

    sends message to admins first and allows redo

--- commands/Competition/random.js
+++ commands/Competition/random.js
@@ -18,6 +18,11 @@ module.exports = {
             return message.channel.send({ embeds: [embed] })
         }
 
+        playersPicked.length = 0
+        for (let x = 0; x < teams.length; x++) {
+            teams[x].team.length = 0
+        }
+
         let playerId
         let teamRoleId
         let randomPlayer
@@ -41,9 +46,28 @@ module.exports = {
             teams[randomTeam].team.push(competition[randomPlayer].object.displayName)
         }
 
+        for (let x = 0; x < teams.length; x++) {
+            let players = []
+
+            for (let y = 0; y < teams[x].team.length; y++) {
+                players.push(teams[x].team[y])
+            }
+
+            const embed = new Discord.MessageEmbed()
+            .setColor(colors.black)
+            .setTitle(`__${teams[x].captainName}\'s Team:__`)
+            .addFields(
+                {name: players[0], value: '\u200B'},
+                {name: players[1], value: '\u200B'},
+                {name: players[2], value: '\u200B'}
+            )
+
+            message.guild.channels.get('841391519775719445').send({ embeds: [embed] })
+        }
+
         const embed = new Discord.MessageEmbed()
         .setColor(colors.green)
-        .setTitle('Teams have been randomly selected! Type !start to begin the competition.')
+        .setTitle('Teams have been randomly selected! Type !start to begin the competition or do !random again for new random teams.')
 
         message.channel.send({ embeds: [embed] })
     }
